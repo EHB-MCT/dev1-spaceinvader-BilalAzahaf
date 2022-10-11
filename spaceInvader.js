5bd0379bb9783d6cb3e250803a99e6ffd21b558a
Bilal Azahaf <bilal.azahaf@student.ehb.be>
2022-10-11 23:19:23 +0200

    Space invader finished

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,24 @@
 "use strict";
 
 alert("Draw your space invader here");
+
+drawProfile();
+
+function drawProfile() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   //B
+context.fillStyle = "purple";
+context.beginPath();
+context.rect(100, 100, 500, 100);
+context.rect(200, 100, 100, 300);
+context.rect(100, 300, 100, 200);
+context.rect(300, 400, 100, 100);
+context.rect(400, 100, 100, 300);
+context.rect(500, 300, 100, 200);
+context.fill();
+}
